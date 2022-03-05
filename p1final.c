041a9e628fa07299f008d0bc3cc255661efe7fe2
varun tej <94589805+Varunteja2@users.noreply.github.com>
2022-03-05 08:47:35 +0000

    done

--- p1final.c
+++ p1final.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+void input(int*num1,int*den1,int*num2,int*den2)
+{ 
+  printf("enter the fraction number");
+  scanf("%d%d%d%d",num1,den1,num2,den2);
+    }
+void add(int num1,int den1,int num2,int den2,int*num3,int*den3)
+{
+  int c;
+  c=den1*den2;
+  *num3=(num1*den2)+(num2*den1);
+  *den3=(c);
+}
+void output(int num1,int den1,int num2,int den2,int num3,int den3)
+{
+  printf("addition of%d/%d+%d/%d=%d/%d",num1,den1,num2,den2,num3,den3);
+}
+int main()
+{
+ int num1,den1,num2,den2,num3,den3;
+ input(&num1,&den1,&num2,&den2);
+ add(num1,den1,num2,den2,&num3,&den3);
+ output(num1,den1,num2,den2,num3,den3);
+  return 0;
+}
--- p2final.c
+++ p2final.c
@@ -1,1 +1,16 @@
 #include<stdio.h>
+struct_fraction
+{
+  int num,den;
+};
+typedef_fraction Fraction;
+fraction input_fraction()
+{
+  fraction f;
+printf("enter one fraction(NUM DEN):\n");
+scanf("%d%d,&f.num,&f.den");
+  return f;
+}
+fraction smallest_fraction(fraction f1,fraction f2,fraction f3)
+{
+  flaot a = (flaot)f1.num/}
